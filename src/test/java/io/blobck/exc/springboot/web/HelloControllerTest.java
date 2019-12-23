637240bbaa5efb8b8a9f090a925112a6f84e010b
hankw <hankwan777@gmail.com>
2019-12-23 22:35:10 +0900

    Dto 테스트

--- src/test/java/io/blobck/exc/springboot/web/HelloControllerTest.java
+++ src/test/java/io/blobck/exc/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -23,5 +23,16 @@ public class HelloControllerTest {
     public void Hello가_리턴된다() throws Exception {
         String hello = "hello~~";
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void getHelloDto() throws Exception {
+        String name = "hello";
+        int amount = 999;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
